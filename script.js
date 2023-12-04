388eb436b11a32eaae75cf96cd1702538e799515
yusuferens <yusuferenketen@gmail.com>
2023-12-04 16:43:31 +0300

    created for loop to render ulEl text

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEL = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
-    console.log(myLeads)
 
 })
 
+for(let i = 0;i < myLeads.length; i++){
+    ulEl.textContent += myLeads[i] + " "
+}
 
