251001a2903e3d2ffdbf9eaecf52d97efbf3be17
yusuferens <yusuferenketen@gmail.com>
2023-12-04 16:58:30 +0300

    created renderLeads function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLeads = [12,34,45]
+let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEL = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
@@ -7,14 +7,16 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
+    renderLeads()
 
 })
 
-let listItems = ""
-
-for(let i = 0;i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li> "
-}
-
-ulEl.innerHTML = listItems
+ function renderLeads(){
+    let listItems = ""
+    for(let i = 0;i < myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li> "
+    }
+    ulEl.innerHTML = listItems
+    
+ }
 
