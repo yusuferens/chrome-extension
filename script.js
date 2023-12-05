a396db329b6027f02dfb07265a7aa78b17280015
yusuferens <yusuferenketen@gmail.com>
2023-12-05 15:43:37 +0300

    modified code to handle console errors

--- script.js
+++ script.js
@@ -1,60 +1,76 @@
-let myLeads = []
-const inputBtn = document.getElementById("input-btn")
-const inputEL = document.getElementById("input-el")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const saveTabBtn = document.getElementById("savetab-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+try {
+    const leadsFromLocalStorageRaw = localStorage.getItem("myLeads");
+    console.log("leadsFromLocalStorageRaw:", leadsFromLocalStorageRaw);
 
-if(leadsFromLocalStorage ) {
-    myLeads = leadsFromLocalStorage;
-    render(myLeads)
+    const leadsFromLocalStorage = leadsFromLocalStorageRaw ? JSON.parse(leadsFromLocalStorageRaw) : [];
+    console.log("leadsFromLocalStorage:", leadsFromLocalStorage);
 
-    
+    if (leadsFromLocalStorage) {
+        myLeads = leadsFromLocalStorage;
+        render(myLeads);
+    }
+} catch (error) {
+    console.error("Error parsing JSON from local storage:", error);
 }
 
-
-saveTabBtn.addEventListener("click", function(){
-    chrome.tabs.query({active: true, currentWindow: true},function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads.value) )
-        render(myLeads)
-    })
-    
-})
-
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        try {
+            if (tabs && tabs.length > 0) {
+                myLeads.push(tabs[0].url);
+                localStorage.setItem("myLeads", JSON.stringify(myLeads));
+                render(myLeads);
+            } else {
+                console.error("No active tab found.");
+            }
+        } catch (error) {
+            console.error("Error handling tabBtn click event:", error);
+        }
+    });
+});
 
 function render(leads) {
-    let listItems = "";
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'> 
-                    ${leads[i]}
-                </a>
-            </li>
-        `;
+    try {
+        let listItems = "";
+        for (let i = 0; i < leads.length; i++) {
+            listItems += `
+                <li>
+                    <a target='_blank' href='${leads[i]}'>
+                        ${leads[i]}
+                    </a>
+                </li>
+            `;
+        }
+        ulEl.innerHTML = listItems;
+    } catch (error) {
+        console.error("Error rendering leads:", error);
     }
-    ulEl.innerHTML = listItems;
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+    try {
+        localStorage.clear();
+        myLeads = [];
+        render(myLeads);
+    } catch (error) {
+        console.error("Error clearing local storage:", error);
+    }
+});
 
-deleteBtn.addEventListener("dblclick",function(){
-   localStorage.clear()
-   myLeads = []
-   render(myLeads)
-   
-})
-
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEL.value)
-    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
-    inputEL.value = "";
-    render(myLeads)
-
-
-})
-
-
+inputBtn.addEventListener("click", function () {
+    try {
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    } catch (error) {
+        console.error("Error handling inputBtn click event:", error);
+    }
+});
